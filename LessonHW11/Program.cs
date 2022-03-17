f350a07158c069e9cd5864b415c91f0ed4f2440c
Dell <Dell@DESKTOP-B6FMJUN>
2022-03-17 14:54:20 +0200

    Добавьте файлы проекта.

---
+++ LessonHW11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LessonHW11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Добавить перегрузку, которая принимает длины двух смежных сторон(double) и величину угла между ними.
+            //Величину угла принимать как int.
+            //Метод должен рассчитывать площадь по формуле:
+            //0.5 * ab * ac * sin(alpha)
+
+
+            formula f = new formula();
+
+            double square1 = f.CalcTriangleSquare(10, 20);
+            double square2 = f.CalcTriangleSquare(15, 30, 1);
+            double square3 = f.CalcTriangleSquare(10, 20, 45);
+
+            Console.WriteLine($"square1:{square1} square2:{square2} square3:{square3}"); ;
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LessonHW11/formula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LessonHW11
+{
+    internal class formula
+    {
+        public double CalcTriangleSquare(double ab, double bc, double ac)
+        {
+            double p = (ab + bc + ac) / 2;
+
+            return Math.Sqrt(p*(p - ab)*(p - bc)*(p - ac));
+        }
+
+        public double CalcTriangleSquare(double b, double h )
+        {
+            return 0.5 * b * h;
+        }
+
+        public double CalcTriangleSquare(double ab, double ac, int alpha)
+        {
+            return 0.5 *ab * ac * Math.Sin(alpha);
+        }
+
+
+
+    }
+}
